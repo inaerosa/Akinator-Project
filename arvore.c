9619faff6ae3800670c672e21da1758a76af1dc1
Inaê Rosa <inaedb.rosa@gmail.com>
2021-08-26 20:08:48 -0300

    Add files via upload

--- arvore.c
+++ arvore.c
@@ -15,11 +15,12 @@ int main(){
  	Arvore * pergunta;
  	Arvore * certa;
  	Arvore * errada;
+ 	Arvore * aux;
  	char resp_certa[50];
 	char resp_errada[50];
 	char respota[3];
 	
-		int verifica_arvore(Arvore * raiz){
+	int verifica_arvore(Arvore * raiz){
 		if (raiz == NULL){
 			return 0;
 		} else {
@@ -30,18 +31,55 @@ int main(){
 	
 	
 	Arvore * inserePerguntaResposta(){
-			
+		if (cont == 0){
 			printf("Informe uma pergunta: ");
+			fflush(stdin);
 	        scanf ("%s", p);  
-	        pergunta = arvore_cria(p); 
+	     
+	       	pergunta = arvore_cria(p); 
 			printf ("Informe a resposta certa: "); 
+			fflush(stdin);
 			scanf ("%s", resp_certa); 
+		
 			pergunta->sim = arvore_cria(resp_certa); 
-			arvore_insere(pergunta, resp_certa);
+//			arvore_insere(pergunta, resp_certa);
 			printf ("Informe a resposta errada: ");
+			fflush(stdin);
 			scanf("%s", resp_errada); 
 			pergunta->nao = arvore_cria(resp_errada); 
-			arvore_insere(pergunta, resp_errada);
+//			arvore_insere(pergunta, resp_errada);
+			cont++;
+		 }else {
+			Arvore * aux = pergunta->nao;
+//			if (aux->nao !=NULL){
+//				for (aux = pergunta->nao; aux->nao != NULL; aux = aux->nao)
+//       		{
+//       			printf("%x %s %x %x\n", aux, aux->info, aux->sim, aux->nao );
+//			   }
+//			}
+       		
+			printf("Informe uma pergunta: ");
+			fflush(stdin);
+        	scanf ("%s", p); 
+			
+       		//criar variavel auxiliar que receba pergunta apontando pra nao?
+       		//aux = arvore_cria(p); 
+			printf ("Informe a resposta certa: ");
+			fflush(stdin); 
+			scanf ("%s", resp_certa); 
+			aux->sim = arvore_cria(resp_certa); 
+//			arvore_insere(aux->sim, resp_certa);
+			printf ("Informe a resposta errada: ");
+			fflush(stdin);
+			scanf("%s", resp_errada); 
+			
+			aux->nao = arvore_cria(resp_errada); 
+			printf ("%s", aux->info);
+			//aux->info tem que ser ornitorrinco
+//			arvore_insere(aux->nao, resp_errada);
+			
+		}
+			
 	
 	}
 	
@@ -56,7 +94,7 @@ int main(){
 		switch (opc){
 			case 1:
 				if (verifica_arvore(pergunta) == 0){
-					printf ("Sou burro\n");
+					printf ("Me ensine alguma coisa :)\n");
 					inserePerguntaResposta();
 				} else {
 					printf("Reproduz o jogo\n");
@@ -67,32 +105,27 @@ int main(){
 						scanf ("%s", resposta);
 						opc = 0;
 					} else if (strcmp(resposta, "nao")== 0){
-						printf ("A resposta eh %s\n", pergunta->nao);
+						printf ("A resposta eh %s?\n", pergunta->nao);
 						scanf ("%s", resposta);
 						if (strcmp(resposta, "sim") == 0) {
-							printf ("Descobri! A resposta eh %s\n", pergunta->sim);
+							printf ("Descobri! A resposta eh %s\n", pergunta->nao);
 							opc = 0;
 						}else if (strcmp(resposta, "nao") == 0){
-							printf ("Isso eh novo para mim, me ensina?");
+							printf ("Isso eh novo para mim, me ensina?\n\n");
 							inserePerguntaResposta();
 						}
 					}
 				}
-		
 			break;
 			case 2:
 				 inserePerguntaResposta();
 			break;
 		}
+		
 	} while (opc != 0);
 	 
 	 printf ("\nAdeus");
-        
-    
-//     printf("\n Altura: %d", arvore_altura(pergunta));
-////     
-//     printf("\n Localizado valor %d", arvore_localiza(a, 'e'));
-//     
+
      system("pause");
           
       return 0;
