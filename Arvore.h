a667910d0415e7e08d955bc037a69058c34409d5
Inaê Rosa <inaedb.rosa@gmail.com>
2021-08-27 00:33:22 -0300

    Add files via upload

--- Arvore.h
+++ Arvore.h
@@ -1,13 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef struct arv Arvore;
-
 struct arv{
        char info[50];
        struct arv * sim; 
        struct arv * nao; 
-       };
+};
+
+
+	int cont=0;
+	char p[50];
+	 	char resp_certa[50];
+	char resp_errada[50];
+	Arvore * pergunta;
+	Arvore * certa;
+	Arvore * errada;
+Arvore * aux;
+
+
+
+
        
 Arvore * arvore_cria(char c[]){
  	Arvore * n = (Arvore *) malloc (sizeof(Arvore));
@@ -22,32 +36,16 @@ Arvore * arvore_cria(char c[]){
   	a->nao = b;
  }
  
- void arvore_imprime (Arvore *a)
- {
- Arvore * p;
- printf("avanca ->%s\n", a->info);
- for (p=a->nao; p!= NULL; p=p->sim)
- {
-  arvore_imprime(p);
- }
- printf("<-- volta\n");
- }
+ void arvore_imprime (Arvore *a){
+ 	Arvore * p;
+ 	printf("avanca ->%s\n", a->info);
+ 	for (p=a->nao; p!= NULL; p=p->sim){
+  		arvore_imprime(p);
+	}
+ 	printf("<-- volta\n");
+}
  
-int arvore_localiza (Arvore *a, char c)
- {
- Arvore * p;
- if (a->info==c)
-    return 1;
- else
- {
- for (p=a->nao; p!= NULL; p=p->sim)
-  {
-  if (arvore_localiza(p,c))
-     return 1;
-   }
-return 0;
-}
-}
+
  
  
  
@@ -63,3 +61,45 @@ return 0;
  }
 return hmax + 1; 
 }
+
+
+Arvore * inserePerguntaResposta(){
+		if (cont == 0){
+			printf("Informe uma pergunta: ");
+			fflush(stdin);
+	        scanf ("%s", p);  
+	       	pergunta = arvore_cria(p); 
+			printf ("Informe a resposta certa: "); 
+			fflush(stdin);
+			scanf ("%s", resp_certa); 
+			pergunta->sim = arvore_cria(resp_certa); 
+			printf ("Informe a resposta errada: ");
+			fflush(stdin);
+			scanf("%s", resp_errada); 
+			pergunta->nao = arvore_cria(resp_errada); 
+			cont++;
+		 }else {
+			Arvore * aux = pergunta->nao;
+			if (aux->nao !=NULL){
+				while(aux->nao != NULL){
+       				aux=aux->nao;
+			   	}	
+			}
+			printf("Informe uma pergunta: ");
+			fflush(stdin);
+        	scanf ("%s", p);     	
+			printf ("Informe a resposta certa: ");
+			fflush(stdin); 
+			scanf ("%s", resp_certa); 
+			aux->sim = arvore_cria(resp_certa); 
+			printf ("Informe a resposta errada: ");
+			fflush(stdin);
+			scanf("%s", resp_errada); 
+	
+			aux->nao = arvore_cria(resp_errada); 
+			
+		}
+	}
+	
+	
+	
--- arvore.c
+++ arvore.c
@@ -4,68 +4,66 @@
 #include "Arvore.h"
 
 
-int main(){
-
-    char p[50];
-   
- 	int cont=0;
  	char resposta[3];
- 	int opc;
- 	Arvore * pergunta;
- 	Arvore * certa;
- 	Arvore * errada;
- 	Arvore * aux;
- 	char resp_certa[50];
-	char resp_errada[50];
-	char respota[3];
-	
-	int verifica_arvore(Arvore * raiz){
-		if (raiz == NULL){
-			return 0;
-		} else {
-			return 1;
-		}
+ 	int opc;	
+ 	
+ 
+
+
+	int jogo (){
+			if (pergunta==NULL){
+					printf ("Me ensine alguma coisa :)\n");
+					inserePerguntaResposta();
+				} else {
+					aux=pergunta;
+					while(aux->nao!=NULL){
+						printf("O que voce esta pensando %s?\n", aux->info);
+						scanf("%s", resposta);
+						if(strcmp(resposta,"sim")==0){
+							printf("eh %s?", aux->sim);
+							scanf ("%s", resposta);
+							if (strcmp(resposta, "sim")== 0){
+								system("cls");
+								printf ("Descobri! A resposta eh %s\n", aux->sim);
+								exit(1);
+							} else if(strcmp(resposta,"nao")==0){
+								printf("A resposta eh %s?\n", aux->nao);
+								scanf("%s", resposta);
+								if(strcmp(resposta,"sim")==0){
+									system("cls");
+									printf ("Descobri! A resposta eh %s\n", aux->nao);
+									exit(1);
+								}
+								
+							}
+							
+						} else if(strcmp(resposta,"nao")==0){
+							printf("A resposta eh %s?\n", aux->nao);
+							scanf("%s", resposta);
+							if(strcmp(resposta,"sim")==0){
+								system("cls");
+								printf ("Descobri! A resposta eh %s\n", aux->nao);
+								exit(1);
+							}
+							
+						}
+						
+					aux = aux->nao;
+					}
+					if (aux->nao==NULL){
+						printf ("Isso eh novo para mim, me ensina?\n\n");
+						inserePerguntaResposta();
+					}
+			
+				}
 	}
+	
+	
+	
+int main(){
 	
 	pergunta = arvore_cria(p); 
-	Arvore * inserePerguntaResposta(){
-		if (cont == 0){
-			printf("Informe uma pergunta: ");
-			fflush(stdin);
-	        scanf ("%s", p);  
-	       	pergunta = arvore_cria(p); 
-			printf ("Informe a resposta certa: "); 
-			fflush(stdin);
-			scanf ("%s", resp_certa); 
-			pergunta->sim = arvore_cria(resp_certa); 
-			printf ("Informe a resposta errada: ");
-			fflush(stdin);
-			scanf("%s", resp_errada); 
-			pergunta->nao = arvore_cria(resp_errada); 
-			cont++;
-		 }else {
-			Arvore * aux = pergunta->nao;
-			if (aux->nao !=NULL){
-			while(aux->nao != NULL)
-       		{
-       			aux=aux->nao;
-			   }
-			}
-			printf("Informe uma pergunta: ");
-			fflush(stdin);
-        	scanf ("%s", p); 
-			printf ("Informe a resposta certa: ");
-			fflush(stdin); 
-			scanf ("%s", resp_certa); 
-			aux->sim = arvore_cria(resp_certa); 
-			printf ("Informe a resposta errada: ");
-			fflush(stdin);
-			scanf("%s", resp_errada); 
-			
-			aux->nao = arvore_cria(resp_errada); 
-			
-		}
-	}
+
 	do{
 		printf("------------AKINATOR-------------\n");
 		printf ("          [0] - Sair            |\n");
@@ -75,37 +73,7 @@ int main(){
 		printf ("--------------------------------\n");
 		switch (opc){
 			case 1:
-				
-				if (pergunta==NULL){
-					printf ("Me ensine alguma coisa :)\n");
-					inserePerguntaResposta();
-				} else {
-					aux=pergunta;
-					while(aux->nao!=NULL){
-						printf("A resposta eh %s?\n", aux->sim);
-						scanf("%s", resposta);
-						if(strcmp(resposta,"sim")==0){
-							system("cls");
-							printf ("Descobri! A resposta eh %s\n", aux->sim);
-							return 0;
-						}
-						else if(strcmp(resposta,"nao")==0){
-							printf("A resposta eh %s?\n", aux->nao);
-							scanf("%s", resposta);
-							if(strcmp(resposta,"sim")==0){
-								system("cls");
-								printf ("Descobri! A resposta eh %s\n", aux->nao);
-								return 0;
-							}
-						}
-						aux=aux->nao;
-					}
-					if (aux->nao==NULL){
-						printf ("Isso eh novo para mim, me ensina?\n\n");
-						inserePerguntaResposta();
-					}
-				}
-				
+				jogo();
 			break;
 			case 2:
 				 inserePerguntaResposta();
